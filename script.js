f6096e28f6ed31d30abe1ece04b2e02e62756fa8
thais-gonzaga <devthaisgonzaga@gmail.com>
2022-08-09 22:01:30 -0300

    adiciona requisitos 7

--- script.js
+++ script.js
@@ -22,6 +22,14 @@ elementp.id = 'answer';
 elementp.innerText = 'Escolha uma cor';
 main.appendChild(elementp);
 
+const divScore = document.createElement('div');
+main.appendChild(divScore);
+
+const pScore = document.createElement('p');
+pScore.innerText = 0;
+pScore.id = 'score';
+divScore.appendChild(pScore);
+
 const button = document.createElement('button');
 button.id = 'reset-game';
 button.innerText = 'JOGAR';
@@ -36,8 +44,14 @@ function random() {
 
 function checkColor(event) {
   const clickColor = event.target.style.background;
-  if (clickColor === p.innerText) elementp.innerText = 'Acertou!';
+  const cont = Number(pScore.innerText);
+  console.log(typeof cont);
+  if (clickColor === p.innerText) {
+    elementp.innerText = 'Acertou!';
+    pScore.innerText = 3 + cont;
+  }
   if (clickColor !== p.innerText) elementp.innerText = 'Errou! Tente novamente!';
+  return cont;
 }
 
 function createDiv() {
@@ -50,21 +64,22 @@ function createDiv() {
   }
 }
 createDiv();
-function teste() {
+
+function textP() {
   const ball = document.querySelectorAll('.ball');
   const sort = Math.round(Math.random() * 5);
   p.innerText = ball[sort].style.background;
   console.log(ball[sort]);
   console.log(sort);
 }
-teste();
+textP();
 
 function play() {
-  teste();
   elementp.innerText = 'Escolha uma cor';
   const div = document.querySelectorAll('.ball');
   for (let index = 0; index < div.length; index += 1) {
     div[index].style.background = random();
   }
+  textP();
 }
 button.addEventListener('click', play);
